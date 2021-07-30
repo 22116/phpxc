07d65c8f417e9b997f9e580378d8cab3c1efc22d
victor.fedorenko <victor.fedorenko@mgid.com>
2021-07-30 20:24:22 +0300

    Added PathResolver to simplify creation in future

--- src/Application/Console/PathResolver.php
+++ src/Application/Console/PathResolver.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace LSBProject\PHPXC\Application\Console;
 
+use JetBrains\PhpStorm\Pure;
+
 final class PathResolver
 {
+    #[Pure]
     public function resolve(string $path): string
     {
         return realpath($path) ?: '';
